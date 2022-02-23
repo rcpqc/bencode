f857b6ed89b7f6cfece3f0cb5193ca7f0639de66
akipeng <akipeng@tencent.com>
2022-02-23 16:04:35 +0800

    test: add case

--- bencode_test.go
+++ bencode_test.go
@@ -18,26 +18,48 @@ type TestS1 struct {
 	XXYh uint32 `bencode:"-"`
 }
 
+type TestS2 struct {
+	GGG   string
+	IFace interface{} `bencode:"iface"`
+}
+
+type TestS3 struct {
+	S1  *TestS1 `bencode:"s1"`
+	SS3 bool
+}
+
 var comTests = []TestCase{
 	{Entity: 23, Data: []byte("i23e")},
 	{Entity: -422, Data: []byte("i-422e")},
 	{Entity: -0, Data: []byte("i0e")},
+	{Entity: uint32(99), Data: []byte("i99e")},
 	{Entity: "abc", Data: []byte("3:abc")},
 	{Entity: "", Data: []byte("0:")},
 	{Entity: "3242434te", Data: []byte("9:3242434te")},
+	{Entity: true, Data: []byte("i1e")},
+	{Entity: false, Data: []byte("i0e")},
 	{Entity: []int{1, 2, 3, 4, 5}, Data: []byte("li1ei2ei3ei4ei5ee")},
 	{Entity: []string{"aa", "b", "ccc"}, Data: []byte("l2:aa1:b3:ccce")},
 	{Entity: []interface{}{"aa", "b", 33, -23, "XX"}, Data: []byte("l2:aa1:bi33ei-23e2:XXe")},
+	{Entity: map[string]int{"aa": 43, "bbbfe": -544, "": 0}, Data: []byte("d0:i0e2:aai43e5:bbbfei-544ee")},
+	{Entity: map[string]interface{}{"a": "1", "b": "2", "c": 3, "d": 4, "e": "5"}, Data: []byte("d1:a1:11:b1:21:ci3e1:di4e1:e1:5e")},
+	{Entity: TestS3{S1: &TestS1{Sf: "gjc", Ffgd: 87}, SS3: true}, Data: []byte("d2:s1d2:sf3:gjc4:ffgdi87ee3:SS3i1ee")},
 }
 
 var encTests = []TestCase{
 	{Entity: TestS1{"xxx", 2, "", 556}, Data: []byte("d2:sf3:xxx4:ffgdi2ee")},
 	{Entity: TestS1{"xxx", 2, "66", 556}, Data: []byte("d2:sf3:xxx4:ffgdi2e3:hhd2:66e")},
+	{Entity: TestS2{GGG: "gggee", IFace: []int{4, 6}}, Data: []byte("d3:GGG5:gggee5:ifaceli4ei6eee")},
+	{Entity: TestS2{GGG: "gggee", IFace: map[string]int{"gds": 8, "353": -45}}, Data: []byte("d3:GGG5:gggee5:ifaced3:353i-45e3:gdsi8eee")},
+	{Entity: [3]int{1, 3, 5}, Data: []byte("li1ei3ei5ee")},
 }
 
 var decTests = []TestCase{
 	{Entity: TestS1{"xxx", 2, "", 0}, Data: []byte("d2:sf3:xxx4:ffgdi2ee")},
 	{Entity: TestS1{"xxx", 2, "66", 0}, Data: []byte("d2:sf3:xxx4:ffgdi2e3:hhd2:664:XXYhi556ee")},
+	{Entity: TestS2{GGG: "gggee", IFace: []interface{}{4, 6}}, Data: []byte("d3:GGG5:gggee5:ifaceli4ei6eee")},
+	{Entity: TestS2{GGG: "gggee", IFace: map[string]interface{}{"gds": 8, "353": -45}}, Data: []byte("d3:GGG5:gggee5:ifaced3:353i-45e3:gdsi8eee")},
+	{Entity: []int{1, 3, 5}, Data: []byte("li1ei3ei5ee")},
 }
 
 func TestMarshal(t *testing.T) {
