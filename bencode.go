c8d017afa5b6cc4d4a699299dd9c521f79bcc1f0
rcpqc <1015272901@qq.com>
2022-02-25 14:51:35 +0800

    style: add comment

--- bencode.go
+++ bencode.go
@@ -5,6 +5,7 @@ import (
 	"reflect"
 )
 
+// Marshal
 func Marshal(v interface{}) ([]byte, error) {
 	buf := bytes.Buffer{}
 	if err := encode(&buf, reflect.ValueOf(v)); err != nil {
@@ -13,6 +14,7 @@ func Marshal(v interface{}) ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
+// Unmarshal
 func Unmarshal(data []byte, v interface{}) error {
 	buf := bytes.NewBuffer(data)
 	if err := decode(buf, reflect.ValueOf(v)); err != nil {
